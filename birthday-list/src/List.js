950791b8fac253f7a04a89a625be0bae93642b6f
taiwo oluwole-young <taiwooluwoleyoung@gmail.com>
2023-07-25 17:03:38 +0100

    Created the useFetch hook

--- birthday-list/src/List.js
+++ birthday-list/src/List.js
@@ -1,14 +1,24 @@
-
+import { useFetch } from './usefetch';
 const List = () => {
     const listCont = {
         title : "My List"
     }
+    const {data: personDetail, error} = useFetch('http://localhost:8000/personDetail');
   return (
-    <div className="Main">
+    <div>
+        {error && <div>{ error }</div>}
+        {personDetail && ( <div className="Main">
         <div className="List">
             <h3> { listCont.title } </h3>
+            { personDetail.map((person) => (
+                <div className="peron-preview" key={person.id}>
+                    <h2>{person.name}</h2>
+                    <p>Written by {person.age}</p>
+                </div>
+            ))}
             <button className="Add-button">Add</button>
         </div>
+    </div>)}
     </div>
   )
 }
---
+++ birthday-list/src/create.jsx
@@ -0,0 +1,10 @@
+
+const Create = () => {
+  return (
+    <div className='create-form'>
+
+    </div>
+  )
+}
+
+export default Create
